bb886641067bcdfde56189a851f21d7d14c3d9c5
hirwajeaneric <hirwaminerve25@gmail.com>
2023-06-02 07:42:31 +0200

    Store and slices configuration over

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { useSelector } from 'react-redux';
+
+function App() {
+  const userList = useSelector((state) => state.users.value);
+
+  return (
+    <div className="App">
+      {""}
+      <div className="addUser">
+        <input type="text" placeholder="Name..." />
+        <input type="text" placeholder="Username..." />
+        <button>Add User</button>
+      </div>
+      <div className="displayUser">
+        {userList.map((user) => {
+          return <h1>{user.name}</h1>
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FakeData.js
@@ -0,0 +1,22 @@
+export const UsersData = [
+    {
+        id: 1, 
+        name: "Hakizimana Joseph",
+        username: "Dcptt",
+    },
+    {
+        id: 2, 
+        name: "Hirwa Jean Eric",
+        username: "Hirwa",
+    },
+    {
+        id: 3, 
+        name: "Kazeneza Lyne Gloria",
+        username: "Gogo",
+    },
+    {
+        id: 4, 
+        name: "Umubyeyi Marie Bienvenue",
+        username: "Umubebe",
+    },
+]
